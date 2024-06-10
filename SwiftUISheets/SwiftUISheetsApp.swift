c40da31439e1b5c77de55d93e90be46161f4c392
Fatih Gumus <fatihgumus72@gmail.com>
2024-06-10 22:43:59 +0300

    Initial Commit

---
+++ SwiftUISheets/SwiftUISheetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISheetsApp.swift
+//  SwiftUISheets
+//
+//  Created by Fatih Gümüş on 10.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISheetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
