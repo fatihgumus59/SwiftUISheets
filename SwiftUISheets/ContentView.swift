865af3236f5184f2a59184ae5341d035041dd43b
Fatih Gumus <fatihgumus72@gmail.com>
2024-06-10 23:42:07 +0300

    sheets

--- SwiftUISheets/ContentView.swift
+++ SwiftUISheets/ContentView.swift
@@ -8,14 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var sheets : Bool = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack{
+            Color.red
+                .ignoresSafeArea(.all)
+            
+            VStack{
+                Button(action: {
+                    sheets.toggle()//tıklandığında değer false dan true ya dönecek.
+                }, label: {
+                   Text("Tıkla")
+                        .foregroundStyle(Color.white)
+                        .frame(width: 100,height: 50)
+                        .background(Color.black)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        
+                })
+//                .sheet(isPresented: $sheets, content: {
+//                    Text("Merhaba Dünya!")
+//                })
+//                .fullScreenCover(isPresented: $sheets, content: {
+//                    ZStack {
+//                        Color.blue
+//                            .ignoresSafeArea(.all)
+//                        Text("Merhaba Dünya!")
+//                            .foregroundStyle(Color.white)
+//                    }
+//                })
+                .fullScreenCover(isPresented: $sheets, content: {
+                    secondScreen()
+                })
+            }
         }
-        .padding()
+    }
+}
+
+struct secondScreen: View{
+    @Environment(\.presentationMode) var presentationMode
+    var body: some View{
+        ZStack(alignment: .topLeading) {
+            
+            Color.blue
+                .ignoresSafeArea(.all)
+                       
+            Button(action: {
+                presentationMode.wrappedValue.dismiss()
+            }, label: {
+                Image(systemName: "xmark")
+                    .foregroundStyle(Color.white)
+                    .font(.largeTitle)
+                    .padding(20)
+            })
+
+      
+        }
+        
     }
 }
 
